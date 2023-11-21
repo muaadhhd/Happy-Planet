55eadc5bf5f754b577a519c0181a434746c39fdf
Solidityarchitect <d12stan@hotmail.com>
2023-11-21 19:56:36 +0700

    first commit

--- backend/contracts/HPToken.sol
+++ backend/contracts/HPToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract HPToken is ERC20 {
     constructor() ERC20("Happy Planet", "HP") {
-        _mint(msg.sender, 100000000000000000000 * 10 ** 18);
+        _mint(msg.sender, 99999999999999999999999 * 10 ** 18);
     }
 }
